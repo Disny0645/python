08cbc9e175baa9a7c9cf84aaafcffb242d9ea087
Disny0645 <57994743+Disny0645@users.noreply.github.com>
2022-08-03 10:53:41 +0200

    Update primeNumbers.py
    Added timing and optimised a bit

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,8 +1,11 @@
+import time
+
 def Count():
+    start = time.time() 
     n = 1
     i = 1
     
-    while i < 1001:  
+    while i < 100001:  
         test = 1
         num_factors = 0
         test_sq = test * test
@@ -20,8 +23,14 @@ def Count():
                   
         if num_factors == 0:
             print(n)
-        i += 1
-        n += 1
+        i += 2
+        n += 2
+
+    end = time.time()
+    timeElaps = end - start
+    timeElaps = round(timeElaps, 3)
+    
+    print("Time Taken:", timeElaps, "Seconds")
 
 
 Count()
